d0f6091d5f04eaa34b3ab09a0a8aee59e9834daf
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-12-07 23:10:55 +0330

    Add And Setup Dot Indicator View Pager

--- app/src/main/java/ir/androidcoder/myapplication/view/MainActivity.kt
+++ app/src/main/java/ir/androidcoder/myapplication/view/MainActivity.kt
@@ -1,5 +1,6 @@
 package ir.androidcoder.myapplication.view
 
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.LinearLayout
@@ -10,6 +11,7 @@ import androidx.viewpager2.widget.ViewPager2
 import dagger.hilt.android.AndroidEntryPoint
 import ir.androidcoder.myapplication.R
 import ir.androidcoder.myapplication.databinding.ActivityDigikalaBinding
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -68,36 +70,57 @@ class MainActivity : AppCompatActivity() {
     private fun imageSlider(data: List<Int>) {
         pagedAdapter = ProductPagerAdapter(data)
         binding.imageView.adapter = pagedAdapter
-        setupDotsIndicator(binding.imageView, binding.dotsLayout, data.size)
+
+        binding.dotsIndicator.attachTo(binding.imageView)
+
+//        setupDotsIndicator(binding.imageView, binding.dotsLayout, data.size)
     }
 
     private fun setupDotsIndicator(viewPager: ViewPager2, dotsLayout: LinearLayout, itemCount: Int) {
-        fun updateDots(currentPosition: Int) {
-            dotsLayout.removeAllViews()
+        val dots = mutableListOf<ImageView>()
 
-            for (i in 0 until itemCount) {
-                val dot = ImageView(viewPager.context).apply {
-                    setImageResource(
-                        if (i == currentPosition) R.drawable.dot_active else R.drawable.dot_inactive
-                    )
-                    val params = LinearLayout.LayoutParams(
-                        LinearLayout.LayoutParams.WRAP_CONTENT,
-                        LinearLayout.LayoutParams.WRAP_CONTENT
-                    )
-                    params.marginEnd = 8
-                    layoutParams = params
-                }
-                dotsLayout.addView(dot)
+        for (i in 0 until itemCount) {
+            val dot = ImageView(viewPager.context).apply {
+                setImageResource(R.drawable.dot_active)
+                val params = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+                )
+                params.marginEnd = 14
+                params.marginStart = 14
+                layoutParams = params
             }
-
+            dotsLayout.addView(dot)
+            dots.add(dot)
         }
 
-        updateDots(viewPager.currentItem)
-
         viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels)
+
+                for (i in dots.indices) {
+                    val scale = when (i) {
+                        position -> 0.9f + 0.5f * (1 - positionOffset)
+                        position + 1 -> 1.0f + 0.5f * positionOffset
+                        else -> 1f
+                    }
+                    dots[i].scaleX = scale
+                }
+            }
+
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
-                updateDots(position)
+                for (i in dots.indices) {
+                    dots[i].setImageResource(
+                        if (i == position) {
+                            R.drawable.dot_active
+                        } else if (i < position) {
+                            R.drawable.last_dot_active
+                        } else {
+                            R.drawable.dot_inactive
+                        }
+                    )
+                }
             }
         })
     }
