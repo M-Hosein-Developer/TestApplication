6819da1a1e1ffe031eff632deed6b14a6700a7ec
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-12-08 15:17:39 +0330

    Setup Detail Activity

--- app/src/main/java/ir/androidcoder/myapplication/coffeeView/DetailActivity.kt
+++ app/src/main/java/ir/androidcoder/myapplication/coffeeView/DetailActivity.kt
@@ -1,5 +1,6 @@
 package ir.androidcoder.myapplication.coffeeView
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
@@ -8,6 +9,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.MultiTransformation
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import ir.androidcoder.myapplication.R
 import ir.androidcoder.myapplication.databinding.ActivityDetailBinding
 import ir.androidcoder.myapplication.util.Constants.EXTRA_PRODUCT_ID
@@ -31,11 +37,11 @@ class DetailActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
 
         //fake data
         val data = intent.extras?.getInt(EXTRA_PRODUCT_ID)
@@ -57,11 +63,21 @@ class DetailActivity : AppCompatActivity() {
 
     }
 
+    @SuppressLint("SetTextI18n")
     private fun setupUi(data: ProductModel) {
+        val transformation = MultiTransformation(CenterCrop(), RoundedCorners(44))
         binding.apply {
-
-
-
+            Glide.with(this@DetailActivity)
+                .load(data.image)
+                .apply(RequestOptions.bitmapTransform(transformation))
+                .into(imgDetailPoster)
+            txtDetailTitle.text = data.title
+            txtDetailDescription.text = data.description
+            txtRate.text = data.rating.toString()
+            txtTotalPrice.text = "$ ${data.price}"
+            btnBuy.setOnClickListener {
+                Toast.makeText(this@DetailActivity, data.title + " add to cart", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
